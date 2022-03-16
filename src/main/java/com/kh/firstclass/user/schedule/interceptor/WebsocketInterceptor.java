3aaa57a18b3fd282800c5b21e36a183b17281e47
손민성 <83879324+Songwanwoo@users.noreply.github.com>
2022-03-16 12:21:06 +0900

    0316민성

---
+++ src/main/java/com/kh/firstclass/user/schedule/interceptor/WebsocketInterceptor.java
@@ -0,0 +1,7 @@
+package com.kh.firstclass.user.schedule.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class WebsocketInterceptor extends HandlerInterceptorAdapter{
+
+}
